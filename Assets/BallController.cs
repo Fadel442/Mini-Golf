ba30e3190a5247e21a57631aaf3d06df30dff848
BlueMoonZ <79092830+Fadel442@users.noreply.github.com>
2023-05-13 11:55:43 +0700

    Level1

--- Assets/BallController.cs
+++ Assets/BallController.cs
@@ -9,7 +9,7 @@ public class BallController : MonoBehaviour, IPointerDownHandler
     [SerializeField] Collider col;
     [SerializeField] Rigidbody rb;
     [SerializeField] float force;
-    //[SerializeField] LineRenderer aimLine;
+    [SerializeField] LineRenderer aimLine;
     [SerializeField] Transform aimWorld;
 
     bool shoot;
@@ -27,23 +27,12 @@ public class BallController : MonoBehaviour, IPointerDownHandler
         {
             if (Input.GetMouseButtonDown(0))
             {
-               // aimLine.gameObject.SetActive(true);
+                aimLine.gameObject.SetActive(true);
                 aimWorld.gameObject.SetActive(true);
                 plane = new Plane(Vector3.up, this.transform.position);
             }
             else if (Input.GetMouseButton(0))
             {
-                var mouseViewportPos = Camera.main.ScreenToViewportPoint(Input.mousePosition);
-                var ballViewportPos = Camera.main.WorldToViewportPoint(this.transform.position);
-                var ballScreenPos = Camera.main.WorldToScreenPoint(this.transform.position);
-                var pointerDirection = ballViewportPos - mouseViewportPos;
-                pointerDirection.z = 0;
-
-                // //draw Aim
-                // aimLine.transform.position = ballScreenPos;
-                // var positions = new Vector3[]{ballScreenPos, Input.mousePosition};
-                // aimLine.SetPositions(positions);
-                 
                 //force direction
                 ray = Camera.main.ScreenPointToRay(Input.mousePosition);
                 plane.Raycast(ray, out var distance);
@@ -51,20 +40,36 @@ public class BallController : MonoBehaviour, IPointerDownHandler
                 forceDirection.Normalize();
 
                 //force factor
+                var mouseViewportPos = Camera.main.ScreenToViewportPoint(Input.mousePosition);
+                var ballViewportPos = Camera.main.WorldToViewportPoint(this.transform.position);
+                var pointerDirection = ballViewportPos - mouseViewportPos;
+                pointerDirection.z = 0;
+                pointerDirection.z *= Camera.main.aspect;
+                pointerDirection.z = Mathf.Clamp(pointerDirection.z, -0.5f, 0.5f);
                 forceFactor = pointerDirection.magnitude * 2; 
-                //Debug.Log(forceFactor);
 
                 //aim visual
                 aimWorld.transform.position = this.transform.position;
                 aimWorld.forward = forceDirection;
                 aimWorld.localScale = new Vector3(1, 1, 0.5f + forceFactor);
 
+                
+                var ballScreenPos = Camera.main.WorldToScreenPoint(this.transform.position);
+                var mouseScreenPos = Input.mousePosition;
+                ballScreenPos.z = 1;
+                mouseScreenPos.z = 1;
+                var positions = new Vector3[]{
+                    Camera.main.ScreenToWorldPoint(ballScreenPos),
+                    Camera.main.ScreenToWorldPoint(mouseScreenPos)
+                };
+                aimLine.SetPositions(positions); 
+                aimLine.endColor = Color.Lerp(Color.blue, Color.red, forceFactor);
             }
             else if (Input.GetMouseButtonUp(0))
             {
                 shoot = true;
                 shootingMode = false;
-                // aimLine.gameObject.SetActive(false);
+                aimLine.gameObject.SetActive(false);
                 aimWorld.gameObject.SetActive(false);
             }
         }
